cc3e909302af3c22dcd0420abe517955987c79ef
yaronid <yaronid@gmail.com>
2017-04-15 12:14:25 -0700

    Init commit

---
+++ Parts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Parts.Startup))]
+namespace Parts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
